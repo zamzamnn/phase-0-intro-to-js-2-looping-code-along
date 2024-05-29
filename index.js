941a857233e9791190c9995480913d358ccf2b89
zamzamnn <zamzamnoorahmed73@gmail.com>
2024-05-29 14:41:30 +0300

    phase-0-intro-to-js-2-looping-code-along lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
